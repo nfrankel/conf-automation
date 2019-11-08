7e9999624ded28d002e9f75d036e845b8203050a
Nicolas Frankel <nicolas@frankel.ch>
2019-11-08 10:00:27 +0100

    Handle acceptance

---
+++ src/main/kotlin/ch/frankel/conf/automation/action/UpdateCalendarEntry.kt
@@ -0,0 +1,20 @@
+package ch.frankel.conf.automation.action
+
+import ch.frankel.conf.automation.AppProperties
+import com.google.api.services.calendar.Calendar
+import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.camunda.bpm.engine.delegate.JavaDelegate
+
+class UpdateCalendarEntry(private val props: AppProperties) : JavaDelegate {
+
+    override fun execute(execution: DelegateExecution) {
+        val client = Calendar
+            .Builder(TRANSPORT, JSON_FACTORY, props.credential)
+            .build()
+        findCalendarEntry(client, props.google, execution.conference)?.let {
+            it.transparency = "opaque"
+            client.events()
+                .update(props.google.calendarId, it.id, it).execute()
+        }
+    }
+}

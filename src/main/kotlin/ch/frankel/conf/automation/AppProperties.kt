007ab0cc42e0e7a9c44d34b170a7c7a4dcceeb5d
Nicolas Frankel <nicolas@frankel.ch>
2021-11-03 09:19:27 +0100

    Migrate property class to regular Kotlin data class

--- src/main/kotlin/ch/frankel/conf/automation/AppProperties.kt
+++ src/main/kotlin/ch/frankel/conf/automation/AppProperties.kt
@@ -1,28 +1,30 @@
 package ch.frankel.conf.automation
 
 import camundajar.impl.com.google.gson.Gson
-import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.*
 
 @ConfigurationProperties("application")
-class AppProperties {
-    lateinit var speaker: String
-    lateinit var name: String
-    val trello = TrelloProperties()
-    val google = GoogleProperties()
-}
+@ConstructorBinding
+data class AppProperties(
+    val speaker: String,
+    val name: String,
+    val trello: TrelloProperties,
+    val google: GoogleProperties
+)
 
-class TrelloProperties {
-    lateinit var key: String
-    lateinit var token: String
-    lateinit var boardShortLink: String
-    lateinit var boardId: String
-    val ips = mutableSetOf<String>()
-}
+data class TrelloProperties(
+    val key: String,
+    val token: String,
+    val boardShortLink: String,
+    val boardId: String,
+    val ips: Set<String>
+)
 
-class GoogleProperties {
-    lateinit var json: String
-    lateinit var sheetId: String
-    lateinit var calendarId: String
+data class GoogleProperties(
+    val json: String,
+    val sheetId: String,
+    val calendarId: String
+) {
     val clientEmail: String by lazy {
         Gson().fromJson(json, Map::class.java)["client_email"] as String
     }

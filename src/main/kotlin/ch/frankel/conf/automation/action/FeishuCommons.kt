566ba4fc2428026fed83279c6cab9f742e4ffa8f
Nicolas Fränkel <nicolas@frankel.ch>
2022-11-25 20:31:14 +0100

    Log WebClient calls for debugging

--- src/main/kotlin/ch/frankel/conf/automation/action/FeishuCommons.kt
+++ src/main/kotlin/ch/frankel/conf/automation/action/FeishuCommons.kt
@@ -1,12 +1,26 @@
 package ch.frankel.conf.automation.action
 
 import ch.frankel.conf.automation.AppProperties
+import io.netty.handler.logging.LogLevel
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.netty.http.client.HttpClient
+import reactor.netty.transport.logging.AdvancedByteBufFormat
+
 
 internal val feishuClient: WebClient
-    get() = WebClient.create("https://open.feishu.cn/open-apis")
+    get() {
+        val httpClient = HttpClient
+            .create()
+            .wiretap("reactor.netty.http.client.HttpClient", LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL)
+        return WebClient
+            .builder()
+            .clientConnector(ReactorClientHttpConnector(httpClient))
+            .baseUrl("https://open.feishu.cn/open-apis")
+            .build()
+    }
 
 internal fun AppProperties.bearerToken(): String? {
     data class TokenResponse(

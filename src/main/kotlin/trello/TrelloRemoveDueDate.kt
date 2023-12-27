6f4cfe87f4baa804618e8e4bcbe71ce5041300ee
Nicolas Fränkel <nicolas@frankel.ch>
2023-12-27 17:13:31 +0100

    fix: Fix bad request response when checking due date on Trello card

--- src/main/kotlin/trello/TrelloRemoveDueDate.kt
+++ src/main/kotlin/trello/TrelloRemoveDueDate.kt
@@ -1,5 +1,6 @@
 package ch.frankel.conf.automation.trello
 
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
@@ -14,11 +15,11 @@ class TrelloRemoveDueDate(private val client: WebClient) {
         try {
             val response = client.put()
                 .uri("/cards/$cardId/dueComplete?key={key}&token={token}")
-                .body(BodyInserters.fromValue("value" to true))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(mapOf("value" to true)))
                 .retrieve()
                 .toBodilessEntity()
                 .block()
-
             logger.info("[$processInstanceId] Due date check result on card $cardId is ${response?.statusCode}")
         } catch (e: WebClientResponseException) {
             logger.info("[$processInstanceId] Due date check failed on card $cardId with status ${e.statusCode}. Most likely the card has no due date")
